0b0dcf3de5101eb23a69c7efc4d48932165bcdbd
Augusto Silva <augustoalves2005@yahoo.com>
2026-05-01 15:23:32 -0300

    Create exercicio23.py

---
+++ Exercicios/exercicio23.py
@@ -0,0 +1,19 @@
+cruzeiro = 0 #Variável definida em 0
+
+while cruzeiro <= 100: #Enquanto cruzeiro for menor ou igual a 0, continue o programa
+
+    cruzeiro +=1 #À variável 0, some sempre 1 número mediante ao anterior para continuar o programa
+
+    if cruzeiro == 13: #Caso o número for 13, você vai fazer isto
+
+        print('Não vou mostrar este número') #Não printe ele
+
+        continue #Depois disso, continue o programa
+
+    print(cruzeiro) #Retome a variável e o programa antes da condição do '13'
+
+    if cruzeiro == 61: #Caso o número der 61, quero que faça isto
+
+        print('Acabou') #Printe acabou
+
+        break #Termine o loop
